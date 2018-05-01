f48f819a77000c9f892034ccdb13d7822ba6b6b0
[Susana Benavidez] <[sbenavid@stanford.edu]>
2018-04-30 21:07:31 -0700

    Initial Commit

---
+++ Swift-Is-Fun/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Swift-Is-Fun
+//
+//  Created by Susana Benavidez on 4/30/18.
+//  Copyright © 2018 Susana Benavidez. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
